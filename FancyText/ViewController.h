19627f82df12d83b5e10e7fce0cf0af627db2754
Duminica Octavian <duminicaoctavian@192-168-0-101.rdsnet.ro>
2018-03-14 18:14:09 +0200

    first commit

--- FancyText/ViewController.h
+++ FancyText/ViewController.h
@@ -8,7 +8,26 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController {
+    CGFloat fontSize;
+    BOOL state;
+}
+
+@property (weak, nonatomic) IBOutlet UILabel *label;
+@property (weak, nonatomic) IBOutlet UITextField *textField;
+@property (weak, nonatomic) IBOutlet UIButton *shadowButton;
+- (IBAction)enterText:(id)sender;
+- (IBAction)red:(id)sender;
+- (IBAction)blue:(id)sender;
+- (IBAction)green:(id)sender;
+- (IBAction)font1:(id)sender;
+- (IBAction)font2:(id)sender;
+- (IBAction)font3:(id)sender;
+- (IBAction)font4:(id)sender;
+- (IBAction)shadow:(id)sender;
+- (IBAction)small:(id)sender;
+- (IBAction)medium:(id)sender;
+- (IBAction)large:(id)sender;
 
 
 @end
--- FancyText/ViewController.m
+++ FancyText/ViewController.m
@@ -17,6 +17,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    fontSize = 30;
+    state = false;
 }
 
 
@@ -26,4 +29,71 @@
 }
 
 
+- (IBAction)enterText:(id)sender {
+    
+    self.label.text = self.textField.text;
+}
+
+- (IBAction)red:(id)sender {
+    self.label.textColor = [UIColor redColor];
+}
+
+- (IBAction)blue:(id)sender {
+    self.label.textColor = [UIColor blueColor];
+}
+
+- (IBAction)green:(id)sender {
+    self.label.textColor = [UIColor colorWithRed:0.0/255.0 green:124.0/255.0 blue:29.0/255.0 alpha:1.0];
+}
+
+- (IBAction)font1:(id)sender {
+    
+    [self.label setFont:[UIFont fontWithName:@"Verdana" size:fontSize]];
+}
+
+- (IBAction)font2:(id)sender {
+    [self.label setFont:[UIFont fontWithName:@"LemonMilk" size:fontSize]];
+}
+
+- (IBAction)font3:(id)sender {
+    [self.label setFont:[UIFont fontWithName:@"Moon Flower" size:fontSize]];
+}
+
+- (IBAction)font4:(id)sender {
+    [self.label setFont:[UIFont fontWithName:@"SugarstyleMillenial-Regular" size:fontSize]];
+}
+
+- (IBAction)shadow:(id)sender {
+    if(state == true) {
+        self.label.layer.shadowOpacity = 0;
+        
+        state = false;
+        
+        [self.shadowButton setTitle:@"Shadow" forState:UIControlStateNormal];
+    } else {
+        self.label.layer.shadowColor = [[UIColor blackColor] CGColor];
+        self.label.layer.shadowOpacity = 0.5;
+        self.label.layer.shadowRadius = 1.0f;
+        self.label.layer.shadowOffset = CGSizeMake(2, 2);
+        
+        state = true;
+        
+        [self.shadowButton setTitle:@"No Shadow" forState:UIControlStateNormal];
+    }
+}
+
+- (IBAction)small:(id)sender {
+    fontSize = 30;
+    self.label.font = [self.label.font fontWithSize:fontSize];
+}
+
+- (IBAction)medium:(id)sender {
+    fontSize = 50;
+    self.label.font = [self.label.font fontWithSize:fontSize];
+}
+
+- (IBAction)large:(id)sender {
+    fontSize = 100;
+    self.label.font = [self.label.font fontWithSize:fontSize];
+}
 @end
